d212c349c3ce3263a69900ac9caa5ff76b0305da
Pedro Ivan Lopez <m@pedroivanlopez.com>
2019-05-07 16:02:56 -0500

    get bookmarks

--- scripts/misc_queries.sql
+++ scripts/misc_queries.sql
@@ -1,5 +1,5 @@
 -- Get tags
-SELECT DISTINCT title
+SELECT *
 FROM moz_bookmarks
 WHERE 1=1
   AND parent IN (
@@ -10,8 +10,14 @@ WHERE 1=1
       AND title = 'Tags')
 ORDER BY title
 
+
 -- Select tagged bookmarks
-SELECT id
+SELECT *
+FROM moz_bookmarks
+WHERE 1=1
+  AND fk IN (
+  
+  SELECT fk
 FROM moz_bookmarks
 WHERE 1=1
   AND parent IN (
@@ -25,3 +31,47 @@ WHERE 1=1
           AND guid = 'tags________'
           AND title = 'Tags')
   )
+ORDER BY id
+  
+  )
+  AND title IS NOT NULL
+
+
+  
+  
+SELECT b.*, p.*
+FROM moz_places p
+INNER JOIN moz_bookmarks b
+  ON b.fk = p.id
+--ORDER  BY p.url
+WHERE b.id IN (
+
+  SELECT id
+  FROM moz_bookmarks
+  WHERE 1=1
+    AND parent IN (
+      SELECT id
+      FROM moz_bookmarks
+      WHERE 1=1
+        AND parent IN (
+          SELECT id
+          FROM moz_bookmarks
+          WHERE 1=1
+            AND guid = 'tags________'
+            AND title = 'Tags')
+    )
+
+)
+
+
+
+SELECT b.fk, b.title , p.url, b.*
+FROM moz_bookmarks b
+  INNER JOIN moz_places p
+ON p.id = b.fk
+--WHERE b.title IS NOT NULL
+ORDER BY p.url, b.title
+	
+SELECT *
+FROM moz_bookmarks
+WHERE fk = 30
