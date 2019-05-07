079f3426844456d661f94075caec114422b31eb5
Pedro Ivan Lopez <m@pedroivanlopez.com>
2019-05-07 13:16:24 -0500

    update queries

--- scripts/misc_queries.sql
+++ scripts/misc_queries.sql
@@ -2,10 +2,26 @@
 SELECT DISTINCT title
 FROM moz_bookmarks
 WHERE 1=1
-  AND parent in (
-	SELECT id
-	FROM moz_bookmarks
-	WHERE 1=1
-	  AND guid = 'tags________'
-	  AND title = 'Tags')
+  AND parent IN (
+    SELECT id
+    FROM moz_bookmarks
+    WHERE 1=1
+      AND guid = 'tags________'
+      AND title = 'Tags')
 ORDER BY title
+
+-- Select tagged bookmarks
+SELECT id
+FROM moz_bookmarks
+WHERE 1=1
+  AND parent IN (
+    SELECT id
+    FROM moz_bookmarks
+    WHERE 1=1
+      AND parent IN (
+        SELECT id
+        FROM moz_bookmarks
+        WHERE 1=1
+          AND guid = 'tags________'
+          AND title = 'Tags')
+  )
