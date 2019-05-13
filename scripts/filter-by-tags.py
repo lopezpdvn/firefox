328fcf3e7ed592b00a1728039cd2a0aae34f6abb
Pedro Ivan Lopez <m@pedroivanlopez.com>
2019-05-13 15:13:07 -0500

    implement filter by tags and command line interface

--- scripts/filter-by-tags.py
+++ scripts/filter-by-tags.py
@@ -6,20 +6,33 @@
 import sys
 import json
 from pprint import pprint
+from argparse import ArgumentParser
 
-
-def get_places_iterator(backup_root):
+def get_tagged_places_iterator(backup_root):
     x = backup_root
     if x.get('type') == 'text/x-moz-place' and x.get('tags'):
         x_ = x.copy()
         x_.pop('children', None)
+        x_['tags'] = set(x_['tags'].split(','))
         yield x_
 
     if 'children' in x and x['children']:
         for child in x['children']:
-            yield from get_places_iterator(child)
+            yield from get_tagged_places_iterator(child)
 
-backup = json.load(sys.stdin)
+parser = ArgumentParser()
+parser.add_argument('-i', '--include-tagged', nargs='*', default=())
+parser.add_argument('-e', '--exclude-tagged', nargs='*', default=())
+args = parser.parse_args()
 
-for i in get_places_iterator(backup):
-    pprint(i)
+include_tagged = set(args.include_tagged)
+exclude_tagged = set(args.exclude_tagged)
+
+places = tuple(get_tagged_places_iterator(json.load(sys.stdin)))
+
+places = [
+    place for place in places
+    if place['tags'] & include_tagged and not place['tags'] & exclude_tagged]
+places.sort(key=lambda place: place['tags'])
+
+pprint(places)
