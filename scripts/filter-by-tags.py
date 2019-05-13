d1c51d6bd488c426a6b8f70c84cce46af262aabc
Pedro Ivan Lopez <m@pedroivanlopez.com>
2019-05-13 14:18:56 -0500

    iterate bookmarks

--- scripts/filter-by-tags.py
+++ scripts/filter-by-tags.py
@@ -5,6 +5,21 @@
 
 import sys
 import json
+from pprint import pprint
+
+
+def get_places_iterator(backup_root):
+    x = backup_root
+    if 'type' in x and x['type'] == 'text/x-moz-place' and 'tags' in x and x['tags']:
+        x_ = x.copy()
+        x_.pop('children', None)
+        yield x_
+
+    if 'children' in x and x['children']:
+        for child in x['children']:
+            yield from get_places_iterator(child)
 
 backup = json.load(sys.stdin)
-print(backup)
+
+for i in get_places_iterator(backup):
+    pprint(i)
